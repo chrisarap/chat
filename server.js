bd021894d7e548deab9251df8a01c7d11b5c53a6
Christian Arteaga <72636990+chrisarap@users.noreply.github.com>
2022-06-16 19:27:38 +0000

    register form added

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser')
+require('dotenv').config();
 
+// middlewares
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// routes
 app.route('/').get((req, res) => {
-  res.send('test');
+  res.sendFile(process.cwd() + '/index.html');
+});
+
+app.route('/register').post((req, res) => {
+  const { username, password } = req.body;
+  res.send('welcome ' + username);
 });
 
 app.listen(3000);
